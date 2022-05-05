0f3da30e6a295c6caa60b3f8095229e83de50e9c
adriaqf <adriaqf@gmail.com>
2022-05-05 10:47:04 +0200

    first commit

---
+++ MasterMind/src/main/java/com/bootcamp/M4/MasterMind/App.java
@@ -0,0 +1,13 @@
+package com.bootcamp.M4.MasterMind;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
